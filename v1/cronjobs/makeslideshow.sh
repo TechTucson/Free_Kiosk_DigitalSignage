53e2198cf6ce5a80c4ba62c920e646f65b9b6b3b
TechTucson <33633261+TechTucson@users.noreply.github.com>
2024-03-19 15:41:53 -0700

    Update makeslideshow.sh

--- v1/cronjobs/makeslideshow.sh
+++ v1/cronjobs/makeslideshow.sh
@@ -1,10 +1,15 @@
 #!bin/bash
-cd /home/spot/temp
-ls /opt/DigitalSignage/imgs> file1.txt
+# Create Listing of Files into file1.txt
+ls /home/spot/html/imgs > file1.txt
+# Add ""
 awk '{ print "\""$0"\""}' file1.txt > file2.txt
-sed '$!s/$/' file2.txt > file3.txt
+#Add Commas
+sed '$!s/$/,/' file2.txt > file3.txt
+#Add All on one line
 paste -s -d "" file3.txt > file4.txt
-
-sed -i '16d' /home/spot/ds/slideshow.html
-media=$(cat /home/spot/temp/file.txt)
+#Delete line 16 to prepare for new Line
+sed -i '16d' /home/spot/html/slideshow.html
+#Add contents of file four to an attribute"
+media=$(cat file4.txt)
+#Add Line 16 with the attribute.  
 sed -i -e "16i$media\\" /home/spot/ds/slideshow.html
